9ec6842ffb57401c01452ce0f804e66a5bb466e2
Pmathie <cykelrytteren@hotmail.com>
2025-01-14 23:25:43 +0100

    Test

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //movementSpeed er en variabel, der bestemmer bevægelseshastigheden.
+    //Vi skriver "public" fordi vi gerne vil kunne tilgå den i Unity Inspector
+    //Vi skriver "float" fordi det er et decimaltal
+    //"movementSpeed" er det navn, vi har givet vores variabel. Vi kunne også kalde den "speed", "x" eller "StopMedAtKiggePaaJeresSkaermeNaarJegGennemgaarTingPaaTavlen"
+   
+
     public float movementSpeed;
    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -17,18 +23,19 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
+
+        if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+            //SLET DETTE OG SKRIV DEN RIGTIGE FUNKTION FOR BEVÆGELSE HER
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
-        }
+
+       
+        //HVAD SKAL DU SKRIVE HER FOR AT BEVÆGE DIG TIL HØJRE, NÅR DU TRYKKER PÅ HØJRE PILETAST?
 
     }
 }
